bd42346e3a1c10974629c6a99ac63f8ba0754561
Zack <lorenx@163.com>
2022-12-01 20:47:08 +0800

    feat: update db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -46,7 +46,7 @@ export const authOptions: NextAuthOptions = {
         user: {
           ...session.user,
           id: user.id,
-          username: user.username,
+          username: user.name,
         },
       };
     },
